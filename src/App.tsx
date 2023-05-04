2b3be5086c8d19079e8e4a958d99725dc693d2a1
karbowskijakub <karbo360@gmail.com>
2023-05-04 21:30:19 +0200

    added apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Header from "./layout/Header";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/graphql",
+  cache: new InMemoryCache(),
+});
 
-function App() {
-  return (
-  <>
-  <div>dsada</div>
-  </>
-  )
+const GET_DATA = gql`
+query GetCountry {
+  country(code: "BR") {
+    name
+    native
+    capital
+    emoji
+    currency
+    languages {
+      code
+      name
+    }
+  }
 }
+`;
+
+const App = () => {
+  const [country, setCountry] = useState();
+
+  const { error, data, loading } = useQuery(GET_DATA, { client });
+  useEffect(() => {
+    data && setCountry(data);
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <>
+        {loading && "Loading"}
+        {error && "error"}
+        <Header />
+      </>
+    </ApolloProvider>
+  );
+};
 
 export default App;
